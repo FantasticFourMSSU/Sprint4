6ece6db58010b24fab49abf20a8085f9a65b5e6d
Bod Teng <PengC001@mymail.mssu.edu>
2020-04-01 00:10:49 -0500

    I put the code in the Login page, you guys can check it too,, I will keep modify the code.

--- LECCISLogin/LECCISLogin.Designer.cs
+++ LECCISLogin/LECCISLogin.Designer.cs
@@ -28,20 +28,74 @@
     /// </summary>
     private void InitializeComponent()
     {
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-      this.SuspendLayout();
-      // 
-      // Login
-      // 
-      resources.ApplyResources(this, "$this");
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-      this.Name = "Login";
-      this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.processorLabel = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.UsernameField = new System.Windows.Forms.TextBox();
+            this.PasswordField = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btnLogin, "btnLogin");
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnExit
+            // 
+            resources.ApplyResources(this.btnExit, "btnExit");
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // processorLabel
+            // 
+            resources.ApplyResources(this.processorLabel, "processorLabel");
+            this.processorLabel.Name = "processorLabel";
+            // 
+            // userNameLabel
+            // 
+            resources.ApplyResources(this.userNameLabel, "userNameLabel");
+            this.userNameLabel.Name = "userNameLabel";
+            // 
+            // UsernameField
+            // 
+            resources.ApplyResources(this.UsernameField, "UsernameField");
+            this.UsernameField.Name = "UsernameField";
+            // 
+            // PasswordField
+            // 
+            this.PasswordField.CausesValidation = false;
+            resources.ApplyResources(this.PasswordField, "PasswordField");
+            this.PasswordField.Name = "PasswordField";
+            // 
+            // Login
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.processorLabel);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.UsernameField);
+            this.Controls.Add(this.PasswordField);
+            this.Name = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
-    #endregion
-  }
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label processorLabel;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.TextBox UsernameField;
+        private System.Windows.Forms.TextBox PasswordField;
+    }
 }
 
--- LECCISLogin/LECCISLogin.cs
+++ LECCISLogin/LECCISLogin.cs
@@ -16,5 +16,45 @@ namespace LECCISLogin
     {
       InitializeComponent();
     }
-  }
+        private MySqlConnection ConnectToDatabase()
+        {
+            string myconnection = "Server= 209.106.201.103; Database=group6; uid=dbstudent14;pwd=spicymonster10";
+            MySqlConnection conn = new MySqlConnection(myconnection);
+            conn.Open();
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                MessageBox.Show("connection opened");
+            }
+            return conn;
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var UN = username.Text;
+            var PW = password.Text;
+            //if (PN == "" || EM == "")
+            //    MessageBox.Show("insert values");
+            //else
+            //{
+            MySqlConnection conn = ConnectToDatabase();
+
+
+            string sql = "INSERT INTO Property(username, password) VALUES (' " + this.username.Text + " ','" + this.password.Text + " ')";
+            using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+            {
+                using (MySqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        MessageBox.Show(string.Format("{0} {1} ", rdr.GetInt32(0), rdr.GetString(1)));
+                    }
+                }
+            }
+            conn.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
 }
