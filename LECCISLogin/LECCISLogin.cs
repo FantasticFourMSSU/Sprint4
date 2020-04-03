b9ad4401165d8b1f8c2bb845e3f588963f047b72
Bod Teng <PengC001@mymail.mssu.edu>
2020-04-02 23:59:47 -0500

    I put some new codes for Login buttom

--- LECCISLogin/LECCISLogin.cs
+++ LECCISLogin/LECCISLogin.cs
@@ -30,27 +30,51 @@ namespace LECCISLogin
         }
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            var UN = username.Text;
-            var PW = password.Text;
-            //if (PN == "" || EM == "")
-            //    MessageBox.Show("insert values");
-            //else
-            //{
-            MySqlConnection conn = ConnectToDatabase();
-
-
-            string sql = "INSERT INTO Property(username, password) VALUES (' " + this.username.Text + " ','" + this.password.Text + " ')";
-            using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+            using (MySqlConnection con = new MySqlConnection(@"Server= 209.106.201.103; Database=group6; uid=dbstudent14;pwd=spicymonster10"))
             {
-                using (MySqlDataReader rdr = cmd.ExecuteReader())
+                MySqlCommand cmd = new MySqlCommand("select * from Users where username like @username and password = @password;");
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Connection = con;
+                con.Open();
+
+                DataSet ds = new DataSet();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(ds);
+                con.Close();
+
+                bool loginSuccessful = ((ds.Tables.Count > 0) && (ds.Tables[0].Rows.Count > 0));
+
+                if (loginSuccessful)
                 {
-                    while (rdr.Read())
-                    {
-                        MessageBox.Show(string.Format("{0} {1} ", rdr.GetInt32(0), rdr.GetString(1)));
-                    }
+                    Console.WriteLine("Success!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password");
                 }
             }
-            conn.Close();
+            //var UN = username.Text;
+            //var PW = password.Text;
+            ////if (PN == "" || EM == "")
+            ////    MessageBox.Show("insert values");
+            ////else
+            ////{
+            //MySqlConnection conn = ConnectToDatabase();
+
+
+            //string sql = "INSERT INTO Property(username, password) VALUES (' " + this.username.Text + " ','" + this.password.Text + " ')";
+            //using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+            //{
+            //    using (MySqlDataReader rdr = cmd.ExecuteReader())
+            //    {
+            //        while (rdr.Read())
+            //        {
+            //            MessageBox.Show(string.Format("{0} {1} ", rdr.GetInt32(0), rdr.GetString(1)));
+            //        }
+            //    }
+            //}
+            //conn.Close();
         }
 
         private void btnExit_Click(object sender, EventArgs e)
