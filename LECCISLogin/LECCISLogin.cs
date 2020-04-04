183f4455286205e7e91a6622437827896a98c73e
Bod Teng <PengC001@mymail.mssu.edu>
2020-04-03 19:59:52 -0500

    I changed Login buttom code.

--- LECCISLogin/LECCISLogin.cs
+++ LECCISLogin/LECCISLogin.cs
@@ -12,7 +12,12 @@ using MySql.Data.MySqlClient;
 namespace LECCISLogin
 {
   public partial class Login : Form
+
   {
+        string passwordFound;
+        public static string usernameInput;
+        public string connectionString = "Fill in with your stuff";
+
     public Login()
     {
       InitializeComponent();
@@ -30,30 +35,69 @@ namespace LECCISLogin
         }
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            using (MySqlConnection con = new MySqlConnection(@"Server= 209.106.201.103; Database=group6; uid=dbstudent14;pwd=spicymonster10"))
+            CheckPassword(connectionString);
+
+            if (password.Text == passwordFound)
             {
-                MySqlCommand cmd = new MySqlCommand("select * from Users where username like @username and password = @password;");
-                cmd.Parameters.AddWithValue("@username", username);
-                cmd.Parameters.AddWithValue("@password", password);
-                cmd.Connection = con;
-                con.Open();
-
-                DataSet ds = new DataSet();
-                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                da.Fill(ds);
-                con.Close();
-
-                bool loginSuccessful = ((ds.Tables.Count > 0) && (ds.Tables[0].Rows.Count > 0));
-
-                if (loginSuccessful)
-                {
-                    Console.WriteLine("Success!");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid username or password");
-                }
+                usernameInput = username.Text;
+                this.Hide();
+                Main_Menu_Page mainMenu = new Main_Menu_Page();
+                mainMenu.ShowDialog();
             }
+
+            else
+            {
+                MessageBox.Show("Login failed. Please verify information.");
+            }
+        }
+
+        private void CheckPassword(string connectionString)
+        {
+            string queryString =
+            $"Select userPassword FROM Employees WHERE userName = '{username.Text}';";
+            using (MySqlConnection connection = new MySqlConnection(
+            connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(
+                queryString, connection);
+                connection.Open();
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        passwordFound = (String.Format("{0}", reader[0]));
+                    }
+                }
+                connection.Close();
+                command.Dispose();
+            }
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //using (MySqlConnection con = new MySqlConnection(@"Server= 209.106.201.103; Database=group6; uid=dbstudent14;pwd=spicymonster10"))
+            //{
+            //    MySqlCommand cmd = new MySqlCommand("select * from Users where username like @username and password = @password;");
+            //    cmd.Parameters.AddWithValue("@username", username);
+            //    cmd.Parameters.AddWithValue("@password", password);
+            //    cmd.Connection = con;
+            //    con.Open();
+
+            //    DataSet ds = new DataSet();
+            //    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            //    da.Fill(ds);
+            //    con.Close();
+
+            //    bool loginSuccessful = ((ds.Tables.Count > 0) && (ds.Tables[0].Rows.Count > 0));
+
+            //    if (loginSuccessful)
+            //    {
+            //        Console.WriteLine("Success!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Invalid username or password");
+            //    }
+            //}
+            ////////////////////////////////////////////////////////
             //var UN = username.Text;
             //var PW = password.Text;
             ////if (PN == "" || EM == "")
